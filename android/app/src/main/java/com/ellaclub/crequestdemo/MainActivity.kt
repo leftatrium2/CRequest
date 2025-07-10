9eb2542f31d2cfae9d35a364edc648ab43f08052
sunxiao5 <sunxiao5@staff.sina.com.cn>
2025-07-10 19:03:33 +0800

    android init version
    android init version

---
+++ android/app/src/main/java/com/ellaclub/crequestdemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ellaclub.crequestdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_main_activity)
+    }
+}
